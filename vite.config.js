b4ef013c7c88b7e10361bc5be32f2a405b9e3bcf
D3adchef <j.marquis504@proton.me>
2025-06-04 00:32:41 -0500

    Fix base so dev works; skip local preview

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Set `base: '/'` in dev, but `'/store/'` in production
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/store/' : '/',
   plugins: [react()],
 })
